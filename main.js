80c8e3edb69fb228af8c66a2eb781dd08ad501e9
VeganVeg <90867314+VeganVeg@users.noreply.github.com>
2024-10-02 19:14:24 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,27 @@ burger.addEventListener('mouseleave', () => {
     gsap.to('.navbar-toggler-icon', { scale: 1, duration: 0.3 })
 })
 
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
 const banniere = document.querySelector('.grid__banniere');
 const btn = document.querySelector('.btn__compris');
 
