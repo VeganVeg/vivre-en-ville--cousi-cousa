ccb9272beacdec7198172910f35b1a73c0b41819
GabrielNBA24 <gabriellolclerval@gmail.com>
2024-11-06 20:05:20 -0500

    services

--- wp-content/themes/vivre-en-ville--cousi-cousa/main.js
+++ wp-content/themes/vivre-en-ville--cousi-cousa/main.js
@@ -11,23 +11,23 @@ burger.addEventListener('mouseleave', () => {
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: true,
-  
+
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
+        el: '.swiper-pagination',
     },
-  
+
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
-  
+
     // And if we need scrollbar
     scrollbar: {
-      el: '.swiper-scrollbar',
+        el: '.swiper-scrollbar',
     },
-  });
+});
 
 const banniere = document.querySelector('.grid__banniere');
 const btn = document.querySelector('.btn__compris');
