06a044f91d1e4e3310ed3be38f513439c3b5ad7e
VeganVeg <90867314+VeganVeg@users.noreply.github.com>
2024-11-25 15:39:14 -0500

    dwa

--- wp-content/themes/vivre-en-ville/header.php
+++ wp-content/themes/vivre-en-ville/header.php
@@ -7,7 +7,33 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+<!--font start-->
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+      rel="stylesheet"
+    />
+    <!--font end-->
+
+    <!--bootstrap start-->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+      crossorigin="anonymous"
+    />
+    <!--bootstrap end-->
+
+    <!--swiper start-->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+    />
+    <!--swiper end-->
+
 <title>
 	<?php bloginfo('name'); // Affiche le nom du blog ?> | 
 	<?php is_front_page() ? bloginfo('description') : wp_title(''); // si nous sommes sur la page d'accueil, affichez la description à partir des paramètres du site - sinon, affichez le titre du post ou de la page. ?>
@@ -35,11 +61,86 @@
 <header>
 		
 
-	<nav>
+	<nav class="navbar navbar-dark navbar-expand-xxl">
 		<?php 
 			// Affiche un menu si dans le tableau de bord un menu a été défini dans cet emplacement
 			wp_nav_menu( array( 'theme_location' => 'main-menu' ) );
 		?>
+		<div class="container-fluid">
+          <!-- BRAND -->
+          <a
+            class="navbar-brand d-flex navbar-brand-mobile d-block d-xxl-none align-items-center"
+            href="index.html"
+            ><img src="sources/logo/logo.png" width="60" height="auto" alt="" />
+          </a>
+
+          <!--hamburger button-->
+          <button
+            class="navbar-toggler border-0 ms-auto"
+            data-bs-toggle="collapse"
+            data-bs-target="#mainNav"
+            aria-controls="mainNav"
+            aria-expanded="false"
+            aria-label="Affichage/masquage de la navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <!--navigation-->
+          <div id="mainNav" class="collapse navbar-collapse">
+            <ul class="navbar-nav mx-auto align-items-center">
+              <li class="nav-item">
+                <a href="news-hub.html" class="nav-link">Nouvelles</a>
+              </li>
+              <li class="nav-item">
+                <a href="services-hub.html" class="nav-link">Services</a>
+              </li>
+              <li class="nav-item">
+                <a href="about.html" class="nav-link">À propos</a>
+              </li>
+
+              <li class="nav-item">
+                <a href="contact-us.html" class="nav-link">Nous joindre</a>
+              </li>
+
+              <li class="nav-item">
+                <a
+                  class="navbar-brand d-flex mx-auto align-items-center d-none d-xxl-flex"
+                  href="index.html"
+                  ><img
+                    src="sources/logo/logo.png"
+                    width="90"
+                    height="auto"
+                    alt=""
+                  />
+                  <div class="title">
+                    VIVRE EN VILLE<br /><span class="subtitle"
+                      >La voie des collectivités viables</span
+                    >
+                  </div>
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a href="team.html" class="nav-link">Équipe</a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">FAQ</a>
+              </li>
+              <li class="nav-item nav-item-don">
+                <a
+                  href="https://activites.vivreenville.org/fr/dons/donate/faire-un-don/2946/"
+                  target="_blank"
+                  class="nav-link nav-link-don"
+                  >Faire un don</a
+                >
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">EN</a>
+              </li>
+            </ul>
+          </div>
+        </div>
 	</nav>
 
 	<?php 
