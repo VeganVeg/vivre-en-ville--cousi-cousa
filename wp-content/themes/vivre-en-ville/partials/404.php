7055dcf053d2f767422f5302e5f3569ede1bd006
willRathier <willi02rm@gmail.com>
2024-11-25 17:15:07 -0500

    update 404

--- wp-content/themes/vivre-en-ville/partials/404.php
+++ wp-content/themes/vivre-en-ville/partials/404.php
@@ -1,4 +1,13 @@
-<section>
-  <h2>404</h2>
-	<img src="https://media.giphy.com/media/3HLMNi9U0yb4CDldVO/giphy.gif" alt="404">
-</section>
+<section class="error">
+        <div class="error__container">
+          <h1>Erreur 404</h1>
+          <h2>
+            Mauvais chemin ... je pense que pour
+            <span class="logo-error">vivre en ville</span> vous deviez tourner a
+            droite.
+          </h2>
+          <div class="gif-404">
+            <iframe src="https://giphy.com/embed/mPytjcsG3XS4o"></iframe>
+          </div>
+        </div>
+      </section>
