24bf128dd0d647186cac22c8b529f002a5d803ac
VeganVeg <90867314+VeganVeg@users.noreply.github.com>
2024-11-25 15:46:46 -0500

    dwedw

--- wp-content/themes/vivre-en-ville/footer.php
+++ wp-content/themes/vivre-en-ville/footer.php
@@ -88,5 +88,14 @@
 	 Vous pouvez la déplacer si désiré, mais garder là. */
 ?>
 
+<script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"
+  ></script>
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
+
 </body>
+
 </html>
