f13773e337bd5dfa356866c50ab464414a594733
VeganVeg <90867314+VeganVeg@users.noreply.github.com>
2024-12-03 17:50:25 -0500

    ht

--- wp-content/themes/vivre-en-ville/main.js
+++ wp-content/themes/vivre-en-ville/main.js
@@ -8,23 +8,6 @@ burger.addEventListener('mouseleave', () => {
     gsap.to('.navbar-toggler-icon', { scale: 1, duration: 0.3 })
 })
 
-
-const banniere = document.querySelector('.grid__banniere');
-const btn = document.querySelector('.btn__compris');
-
-btn.addEventListener('click', () => {
-    banniere.classList.add('grid__banniere--none')
-});
-
-const don = document.querySelector('.btn__don')
-
-don.addEventListener('mouseenter', () => {
-    gsap.timeline()
-        .to('.heart', { scale: 1.2, duration: 0.3, ease: 'back.in' })
-        .to('.heart', { scale: 1, duration: 0.3 })
-})
-
-
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: true,
@@ -45,3 +28,33 @@ const swiper = new Swiper('.swiper', {
         el: '.swiper-scrollbar',
     },
 });
+
+const banniere = document.querySelector('.grid__banniere');
+const btn = document.querySelector('.btn__compris');
+
+function updateButton() {
+    const isClicked = localStorage.getItem('buttonState') === 'true';
+
+    if (isClicked) {
+        banniere.classList.add('grid__banniere--none');
+
+    }
+}
+
+
+btn.addEventListener('click', () => {
+    localStorage.setItem('buttonState', 'true')
+
+    updateButton();
+});
+
+
+window.onload = updateButton;
+
+const don = document.querySelector('.btn__don')
+
+don.addEventListener('mouseenter', () => {
+    gsap.timeline()
+        .to('.heart', { scale: 1.2, duration: 0.3, ease: 'back.in' })
+        .to('.heart', { scale: 1, duration: 0.3 })
+})
