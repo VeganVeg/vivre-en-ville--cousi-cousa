22fbb5e12e19f063b880357fa6caafe7bdf6c943
VeganVeg <90867314+VeganVeg@users.noreply.github.com>
2024-12-05 12:20:06 -0500

    fes

--- wp-content/themes/vivre-en-ville/services-hub.php
+++ wp-content/themes/vivre-en-ville/services-hub.php
@@ -1,7 +1,7 @@
 <?php 
 /**
  * 	Template Name: Tous les services
- * 	Template Post Type: page, post, article
+ * 	Template Post Type: page, post, article, service
  */
 
 get_header(); // Affiche header.php
