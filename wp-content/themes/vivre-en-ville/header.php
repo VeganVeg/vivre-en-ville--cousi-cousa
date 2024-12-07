8568af4d86b886cc554a5854491a5968e4f20b88
VeganVeg <90867314+VeganVeg@users.noreply.github.com>
2024-12-07 17:44:34 -0500

    adw

--- wp-content/themes/vivre-en-ville/header.php
+++ wp-content/themes/vivre-en-ville/header.php
@@ -62,6 +62,23 @@
 >
 
 <header>
+
+<!--banniere-->
+<section class="banniere">
+        <div class="grid__banniere">
+          <div class="banniere__txt">
+            <span
+              >Ce site web sert de projet scolaire et n'est en aucun cas le vrai
+              site Vivre en Ville. Pour vous rendre sur le vrai site cliquez sur
+              ce lien :<a href="https://vivreenville.org/">
+                https://vivreenville.org/</a></span>
+          </div>
+
+          <div class="banniere__btn">
+            <button class="btn__compris">J'ai compris</button>
+          </div>
+        </div>
+      </section>
 		
 
 	<nav class="navbar navbar-dark navbar-expand-xxl">
